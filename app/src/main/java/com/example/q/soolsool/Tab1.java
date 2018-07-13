cf4b2438b7f9d336ee1a66edb87d6d7fffc3451e
ON Kim <36390237+soungonkim@users.noreply.github.com>
2018-07-13 22:49:10 +0900

    layouting

--- app/src/main/java/com/example/q/soolsool/Tab1.java
+++ app/src/main/java/com/example/q/soolsool/Tab1.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,42 @@ public class Tab1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab1, container, false);
+        View view = inflater.inflate(R.layout.tab1, container, false);
+        RecyclerView rec_view = view.findViewById(R.id.tab1_rec);
+
+
+
+
+
+
+    }
+
+    class Tab1Adapter extends RecyclerView.Adapter<Tab1Adapter.Viewholder>{
+        @NonNull
+        @Override
+        public Viewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull Viewholder viewholder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        class Viewholder extends RecyclerView.ViewHolder{
+
+            public Viewholder(@NonNull View itemView) {
+                super(itemView);
+
+            }
+        }
+
+
+
     }
 }
