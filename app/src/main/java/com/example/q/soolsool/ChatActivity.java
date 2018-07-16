c418a5c1a5db29ebc3cd4926dd27151e83fcbc28
tommyyun <sjyun@kaist.ac.kr>
2018-07-16 23:28:02 +0900

    07. 16. 23:30

--- app/src/main/java/com/example/q/soolsool/ChatActivity.java
+++ app/src/main/java/com/example/q/soolsool/ChatActivity.java
@@ -2,6 +2,7 @@ package com.example.q.soolsool;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
